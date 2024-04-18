367c54f7c20d4b07d69db916f6a0dd624ed3f2ca
Maja Bojarska <mabojars@akamai.com>
2024-04-18 13:12:48 +0200

    lvim config save

--- dotfiles/.config/lvim/config.lua
+++ dotfiles/.config/lvim/config.lua
@@ -2,3 +2,12 @@
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
+
+reload('user.plugins')
+reload('user.colorscheme')
+reload('user.options')
+reload('user.keymaps')
+reload('user.which-key')
+reload('user.statusline')
+reload('user.lsp')
+reload('user.dashboard')
